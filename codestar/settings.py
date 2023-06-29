7fbc77c52f0923fccf6814ce8f278f456014bbff
rohina usman <rohina.usman@gmail.com>
2023-06-29 13:19:32 +0000

    urls added

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-# ALLOWED_HOSTS = ['8000-ru2214-blogprojectdjang-8prmvwhth99.ws-eu100.gitpod.io']
-ALLOWED_HOSTS = ['codestar-myblog-db0da9aab729.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-myblog-db0da9aab729.herokuapp.com', 'localhost','8000-ru2214-blogprojectdjang-8prmvwhth99.ws-eu101.gitpod.io']
+# ALLOWED_HOSTS = ['codestar-myblog-db0da9aab729.herokuapp.com', 'localhost']
 
 # Application definition
 

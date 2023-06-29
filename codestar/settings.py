10d455daacc42c1ed7cbbbed1886b9afd0fef238
rohina usman <rohina.usman@gmail.com>
2023-06-29 13:48:13 +0000

    post details added

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['codestar-myblog-db0da9aab729.herokuapp.com', 'localhost','8000-ru2214-blogprojectdjang-8prmvwhth99.ws-eu101.gitpod.io']
 # ALLOWED_HOSTS = ['codestar-myblog-db0da9aab729.herokuapp.com', 'localhost']
 
